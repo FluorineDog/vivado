474a2206a0f93571b34b7f50adae46ec011955d8
fluorinedog <fluorinedog@gmail.com>
2017-05-31 14:50:24 +0800

    checkpoint

--- counter/counter.srcs/sources_1/new/main.v
+++ counter/counter.srcs/sources_1/new/main.v
@@ -21,6 +21,27 @@
 
 
 module main(
+  input clk,
+  input clear,
+  input M,
+  input base[3:0],
+  input LD, 
+  output Q[3:0], 
   );
-
+  reg QS[4:0];
+  assign Q[3:0] = QS[3:0];
+  always @ (posedge clk or negedge clear or negedge LD) begin
+    if(clear==0)
+      QS[3:0] <= 4'b0;
+    else if (LD==0)
+      QS[3:0] <= base[3:0];
+    else begin
+      if(M==0)
+        QS <= QS + 1;
+      else 
+        QS <= QS - 1;
+    end
+  always @ (posedge Q[4] or negedge clk) begin
+    
+  end
 endmodule
