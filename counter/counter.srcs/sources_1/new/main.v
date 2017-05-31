0eed404cc022bec0e01913ac96be145ff493dcdf
fluorinedog <fluorinedog@gmail.com>
2017-05-31 14:07:51 +0800

    reconstruct

---
+++ counter/counter.srcs/sources_1/new/main.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/31/2017 01:55:04 PM
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main(
+  );
+
+endmodule
