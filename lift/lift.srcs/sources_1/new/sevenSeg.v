e20cbfcbc1247caccefd335e6899a142a713ce1c
fluorinedog <fluorinedog@gmail.com>
2017-06-06 15:38:45 +0800

    all

--- lift/lift.srcs/sources_1/new/sevenSeg.v
+++ lift/lift.srcs/sources_1/new/sevenSeg.v
@@ -22,8 +22,34 @@
 
 module sevenSeg(
   input [2:0] lift_floor, 
-  input [3:0] timeing,     
-  input 
+  input [3:0] timer,     
+  input clk,      // require : 1ms-4ms period
+  output reg [7:0] AN,
+  output reg [7:0] seg_data
   );
+  reg [2:0] choose;
+  reg [7:0] lift_floor_seg;
+  reg [7:0] timer_seg;
+  integer i;  
 
+  sevenSegDigit ssd0({1b'0, lift_floor},lift_floor_seg);
+  sevenSegDigit ssd1(timer, timer_seg);
+
+  initial begin
+    choose = 0;
+  end
+
+  always @(*) begin
+    for(i=0;i<8;++i)begin
+      AN[i]=(choose!=i)
+    end
+    case(choose)
+      'h0:     seg_data = lift_floor_seg;
+      'h7:     seg_data = timer_seg;
+      default  seg_data = 8'b11111101;
+  end
+  
+  always @(posedge clk) begin
+    choose <= choose + 1;
+  end
 endmodule
--- lift/lift.srcs/sources_1/new/sevenSegDigit.v
+++ lift/lift.srcs/sources_1/new/sevenSegDigit.v
@@ -26,23 +26,23 @@ module sevenSegDigit(
   );
   always @(digit)
   case (digit)
-      4'h0: seg_data = 8'b0000001;
-      4'h1: seg_data = 8'b1001111;
-      4'h2: seg_data = 8'b0010010;
-      4'h3: seg_data = 8'b0000110;
-      4'h4: seg_data = 8'b1001100;
-      4'h5: seg_data = 8'b0100100;
-      4'h6: seg_data = 8'b0100000;
-      4'h7: seg_data = 8'b0001111;
-      4'h8: seg_data = 8'b0000000; 
-      4'h9: seg_data = 8'b0000100;
-      4'hA: seg_data = 8'b0001000;
-      4'hb: seg_data = 8'b1100000;
-      4'hC: seg_data = 8'b0110001;
-      4'hd: seg_data = 8'b1000010;
-      4'hE: seg data = 8'b0110000;
-      4'hF: seg data = 8'b0111000;
+      4'h0: seg_data = 8'b00000011;
+      4'h1: seg_data = 8'b10011111;
+      4'h2: seg_data = 8'b00100101;
+      4'h3: seg_data = 8'b00001101;
+      4'h4: seg_data = 8'b10011001;
+      4'h5: seg_data = 8'b01001001;
+      4'h6: seg_data = 8'b01000001;
+      4'h7: seg_data = 8'b00011111;
+      4'h8: seg_data = 8'b00000001; 
+      4'h9: seg_data = 8'b00001001;
+      4'hA: seg_data = 8'b00010001;
+      4'hb: seg_data = 8'b11000001;
+      4'hC: seg_data = 8'b01100011;
+      4'hd: seg_data = 8'b10000101;
+      4'hE: seg data = 8'b01100001;
+      4'hF: seg data = 8'b01110001;
    default: 
-            seg_data = 8'b0000001;
+            seg_data = 8'b11111101;
   endcase
 endmodule
